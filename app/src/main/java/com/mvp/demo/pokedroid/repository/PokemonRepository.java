195ed698c76305f36fcaa25261d7d4e21853f47f
Jorge Atempa <atempaitm@gmail.com>
2018-04-06 00:45:48 -0700

    Crear repositorio

---
+++ app/src/main/java/com/mvp/demo/pokedroid/repository/PokemonRepository.java
@@ -0,0 +1,22 @@
+package com.mvp.demo.pokedroid.repository;
+
+import com.mvp.demo.pokedroid.api.PokemonService;
+import com.mvp.demo.pokedroid.model.PokemonList;
+
+import javax.inject.Inject;
+
+import io.reactivex.Observable;
+
+public class PokemonRepository {
+    private static final int MAX = 20;
+    private PokemonService service;
+
+    @Inject
+    public PokemonRepository(PokemonService service) {
+        this.service = service;
+    }
+
+    public Observable<PokemonList> getPokemons(int offset) {
+        return service.getPokemonList(MAX, offset);
+    }
+}
