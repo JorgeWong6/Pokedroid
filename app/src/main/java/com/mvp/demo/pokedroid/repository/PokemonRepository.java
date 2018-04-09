ada8fc5289d7242cf7e18afac5ccdfa99238a127
Jorge Atempa <atempaitm@gmail.com>
2018-04-08 23:01:17 -0700

    Update repository class

--- app/src/main/java/com/mvp/demo/pokedroid/repository/PokemonRepository.java
+++ app/src/main/java/com/mvp/demo/pokedroid/repository/PokemonRepository.java
@@ -4,9 +4,13 @@ import com.mvp.demo.pokedroid.api.PokemonService;
 import com.mvp.demo.pokedroid.model.PokemonList;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
 import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
+@Singleton
 public class PokemonRepository {
     private PokemonService service;
 
@@ -16,6 +20,8 @@ public class PokemonRepository {
     }
 
     public Observable<PokemonList> getPokemons(int max, int offset) {
-        return service.getPokemonList(max, offset);
+        return service.getPokemonList(max, offset)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
     }
 }
