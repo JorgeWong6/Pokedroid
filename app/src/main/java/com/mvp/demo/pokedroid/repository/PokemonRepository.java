b7b145ea07ba0daf7d43b41de7fc5cc64f2cb9ef
Jorge Atempa <atempaitm@gmail.com>
2018-04-18 09:46:24 -0700

    Fetch data on demand

--- app/src/main/java/com/mvp/demo/pokedroid/repository/PokemonRepository.java
+++ app/src/main/java/com/mvp/demo/pokedroid/repository/PokemonRepository.java
@@ -1,5 +1,7 @@
 package com.mvp.demo.pokedroid.repository;
 
+import android.util.Log;
+
 import com.mvp.demo.pokedroid.api.PokemonService;
 import com.mvp.demo.pokedroid.database.PokemonDao;
 import com.mvp.demo.pokedroid.model.Pokemon;
@@ -7,6 +9,8 @@ import com.mvp.demo.pokedroid.model.PokemonList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -20,29 +24,42 @@ import io.reactivex.schedulers.Schedulers;
 public class PokemonRepository {
     private final PokemonService service;
     private final PokemonDao dao;
+    private final Executor executor;
 
     @Inject
     public PokemonRepository(PokemonService service, PokemonDao dao) {
         this.service = service;
         this.dao = dao;
+        this.executor = Executors.newSingleThreadExecutor();
     }
 
-    public Flowable<List<Pokemon>> getPokemons(int max, int offset) {
-        service.getPokemonList(max, offset)
-               .subscribeOn(Schedulers.io())
-               .observeOn(Schedulers.newThread())
-               .subscribe(new Consumer<PokemonList>() {
-                   @Override
-                   public void accept(PokemonList pokemonList) throws Exception {
-                       ArrayList<Pokemon> pokemons = pokemonList.getResults();
-                       for (Pokemon pokemon : pokemons) {
-                           dao.insert(pokemon);
-                       }
-                   }
-               });
-
-        return dao.getAll()
+    public Flowable<List<Pokemon>> getPokemons(int max, final int offset) {
+        fetchData(max, offset);
+        return dao.getAll(max, offset)
                   .subscribeOn(Schedulers.io())
                   .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    private void fetchData(int max, int offset) {
+        service.getPokemonList(max, offset)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.newThread())
+                .subscribe(new Consumer<PokemonList>() {
+                    @Override
+                    public void accept(PokemonList pokemonList) throws Exception {
+                        ArrayList<Pokemon> pokemons = pokemonList.getResults();
+
+                        for (Pokemon pokemon : pokemons) {
+                            boolean pokemonExists = (dao.hasPokemon(pokemon.getName()) != null);
+
+                            if (!pokemonExists) {
+                                dao.insert(pokemon);
+                                Log.d("DB", "SIZE " + dao.getTotal());
+                            }
+
+                            Log.d("REPOSITORY", "SIZE " + dao.getTotal());
+                        }
+                    }
+                });
     }
 }
